675616f3e41aef9436ca7ebc8c0ae4825e5b0c77
jayanta-hub <76052991+jayanta-hub@users.noreply.github.com>
2024-09-29 16:38:21 +0530

    refactor

--- src/Infrastructure/utils/screenUtility.ts
+++ src/Infrastructure/utils/screenUtility.ts
@@ -3,9 +3,9 @@ import {Dimensions, Platform} from 'react-native';
 const {width, height} = Dimensions.get('window');
 const guidelineBaseWidth = Platform.isPad ? 600 : 375;
 const guidelineBaseHeight = Platform.isPad ? 1024 : 812;
-const scale = size => (width / guidelineBaseWidth) * size;
-const verticalScale = size => (height / guidelineBaseHeight) * size;
-const moderateScale = (size, factor = 0.5) =>
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor = 0.5) =>
   size + (scale(size) - size) * factor;
 
 export {
